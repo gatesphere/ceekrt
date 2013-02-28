e95a0ea387d266b76cb00c850080f77ae941bf7c
Jacob Peck <peckj@upstate.edu>
2013-02-28 14:14:30 -0500

    Templates + static dir.

--- ceekrt/views.py
+++ ceekrt/views.py
@@ -1,8 +1,9 @@
 #@+leo-ver=5-thin
 #@+node:peckj.20130227152029.1396: * @file views.py
 from ceekrt import app
+from flask import render_template
 
 @app.route('/')
 def index():
-  return 'Hello World!'
+  return render_template('index.html')
 #@-leo
